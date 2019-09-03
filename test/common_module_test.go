8a766870dcd0983cf469571ddc9444e8d256a2a5
gitName <a@b.c>
2019-09-03 11:16:24 +0200

    add test for keyvault creation

--- test/common_module_test.go
+++ test/common_module_test.go
@@ -1,8 +1,13 @@
 package test
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/mgmt/2018-02-14/keyvault"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
@@ -17,5 +22,31 @@ func TestCommonModule(t *testing.T) {
 	defer terraform.Destroy(t, terraformFixtureOptions)
 	terraform.InitAndApply(t, terraformFixtureOptions)
 
-	// todo: test
+	resourceGroupName := "tf-ref-common-rg"
+	keyVaultName := "tf-ref-common-kv"
+
+	// check the keyvault is deployed
+	err := testKeyVault(resourceGroupName, keyVaultName)
+	if err != nil {
+		t.Fatalf("KeyVault test has failed: %e", err)
+	}
+}
+
+func testKeyVault(resourceGroupName string, vaultName string) error {
+	AzureSubscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+
+	kvClient := keyvault.NewVaultsClient(AzureSubscriptionID)
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err == nil {
+		kvClient.Authorizer = authorizer
+	} else {
+		return fmt.Errorf("Cannot get an Azure SDK Authorizer: %v", err)
+	}
+
+	_, err = kvClient.Get(context.Background(), resourceGroupName, vaultName)
+	if err != nil {
+		return fmt.Errorf("Cannot retrieve Azure Keyvault with name %s in resource group %s: %v", vaultName, resourceGroupName, err)
+	}
+
+	return nil
 }
